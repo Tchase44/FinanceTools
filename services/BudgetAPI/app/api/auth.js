185ddc1a3a46b2f7def5805448f0d5fd75ef1531
Tchase44 <tchase4544@gmail.com>
2018-01-08 10:33:45 -0500

    more auth

--- services/BudgetAPI/app/api/auth.js
+++ services/BudgetAPI/app/api/auth.js
@@ -1,8 +1,32 @@
-const mongoose = require('mongooose'),
-      UserModel = require('@BudgetManagerModels/user');
+const mongoose = require('mongoose'),
+      jwt = require('jsonwebtoken'),
+      config = require('@config');
 
-const models = {
-    User: mongoose.model('User')
+const api = {}
+// two '=' signs ??
+api.login = (User) = (req,res) => {
+    User.findOne({username: req.body.username}, (error,user)=>{
+        if(error){ throw error};
+        if(!user){
+            res.status(401).send({success:false,message: 'Authentication failed. user not found'});
+        }else{
+            user.comparePassword(req.body.password, (error,matches)=>{
+                if(matches && !error){
+                    const token = jwt.sign({user},config.secret);
+                    res.json({success: true, message: 'Token granted', token})
+                }else{
+                    res.status(401).send({success: false, message:'Authentication failed, worng password.'})
+                }
+            })
+        }
+    })
 }
-
-module.exports = models;
+api.verify = (headers) => {
+    if(headers && headers.autherization){
+        const split = headers.autherization.split(' ');
+        if(split.length === 2){return split[1]}
+        else{ return null}
+    }else{
+        return null
+    }
+}
