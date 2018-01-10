e2998050b13c28c6cf5eefb58dc2d6ae7d5b79f2
Tchase44 <tchase4544@gmail.com>
2018-01-10 13:07:17 -0500

    budget vues

---
+++ application/src/components/Budget/BudgetListBody.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="1-budget-body">
+    	<div class="md-budget" v-if="budgets != null" v-for="budget in budgets">
+    		<div class="md-budget-info white--text">{{budget.client}}</div>
+    		<div class="md-budget-info white--text">{{budget.title}}</div>
+    		<div class="md-budget-info white--text">{{budget.state}}</div>
+    	    <div class="1-budget-actions">
+                <v-btn small flat color="light-blue lighten-1">
+                    <v-icon small>visibility</v-icon>
+                </v-btn>
+                <v-btn small flat color="yellow accent-1">
+                    <v-icon>mode_edit</v-icon>
+                </v-btn>
+                <v-btn small flat color="red lighten-1">
+                    <v-icon>delete_forever</v-icon>
+                </v-btn>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  props: ['budgets']
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
