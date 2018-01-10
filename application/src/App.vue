e5dd061c43f7b7079433cf692d3de7b6cb1bc22a
Tchase44 <tchase4544@gmail.com>
2018-01-10 12:08:50 -0500

    added image and body styles

--- application/src/App.vue
+++ application/src/App.vue
@@ -15,18 +15,22 @@ export default {
 <style lang="scss">
 @import "./assets/styles";
 
-// body{
-//   // background: url("./assets/images/backy.jpg") no-repeat center center fixed;
-//   &:after{
-//     content: '';
-//     position: fixed;
-//     width: 100%;
-//     height: 100%;
-//     top: 0;
-//     left: 0;
-//     background-color: $background-tint;
-//     opacity: .3;
-//     z-index:-1;
-//   }
-// }
+body{
+  background: url("./assets/images/backy.jpeg") no-repeat center center fixed;
+  background-size: cover;
+  &:after{
+    content: '';
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: $background-tint;
+    opacity: .3;
+    z-index:-1;
+  }
+  .application {
+      background: none;
+    }
+}
 </style>
