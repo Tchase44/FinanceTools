22e215cba4979cae8744a9c3a3bda61de30d0037
Tchase44 <tchase4544@gmail.com>
2018-01-10 13:36:16 -0500

    refactor

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -1,41 +1,39 @@
 const mongoose = require('mongoose');
-// const User = mongoose.model('User');
 
 const api = {};
 // should not exist in production
-api.setup = (User) => (req, res) => {
-    const admin = new User({
-        username: 'bender',
-        password: 'bender',
-        clients: []
-    });
-    admin.save(error => {
-        if (error) throw error;
-        console.log('Admin account was succesfully set up');
-        res.json({ success: true });
-    })
-}
+// api.setup = (User) => (req, res) => {
+//     const admin = new User({
+//         username: 'bender',
+//         password: 'bender',
+//         clients: []
+//     });
+//     admin.save(error => {
+//         if (error) throw error;
+//         console.log('Admin account was succesfully set up');
+//         res.json({ success: true });
+//     })
+// }
 // end
-api.index = (User, BudgetToken) => (req, res) => {
-    const token = BudgetToken;
-    if (token) {
-        User.find({}, (error, users) => {
-            if (error) throw error;
-            res.status(200).json(users);
-        });
-    } else return res.status(403).send({ success: false, message: 'Unauthorized' });
-}
+// api.index = (User, BudgetToken) => (req, res) => {
+//     const token = BudgetToken;
+//     if (token) {
+//         User.find({}, (error, users) => {
+//             if (error) throw error;
+//             res.status(200).json(users);
+//         });
+//     } else return res.status(403).send({ success: false, message: 'Unauthorized' });
+// }
 
 api.signup = (User) => (req,res) =>{
     if(!req.body.username || !req.body.password){
         res.json({success:false,message:'need username and password'})
     }else{
-        const newUser = new User({
+        const user = new User({
             username: req.body.username,
             password: req.body.password,
-            clients: []
         })
-        newUser.save((error)=>{
+        user.save((error)=>{
             if(error){
                 return res.status(400).json({success:false,message:'username exists'})
             }
